b272299970171cb9ebfbcf0ce7723aa1ca1ecf10
diana3664 <dianamus2017@gmail.com>
2021-04-18 17:59:06 +0300

    add a paragraph to our object

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ $("#services1").click(function() {
 });
 
 $(document).ready(function() {
-  $("#services1").click(function() {
-   $("#services1").slideDown().hide();
-   $("#design").show();
-   $("#design").click(function() {
-    $("#design").slideUp('1500');
-    $("#services1").slideDown('1500');
+  $("#services2").click(function() {
+   $("#services2").slideDown().hide();
+   $("#development").show();
+   $("#development").click(function() {
+    $("#development").slideUp();
+    $("#services2").slideDown();
    })
   });
   });
+  
   
