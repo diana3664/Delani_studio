2a6465e8b0d729652198a89bca27eb64db01464d
diana3664 <dianamus2017@gmail.com>
2021-04-18 17:42:45 +0300

    add a styling to the paragrhaph with id development

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $("#services1").click(function() {
- $("#services1").slideDown("1000").hide();
+ $("#services1").slideDown().hide();
  $("#design").show();
  $("#design").click(function() {
   $("#design").slideUp('1500');
@@ -9,3 +9,14 @@ $("#services1").click(function() {
 });
 });
 
+$(document).ready(function() {
+  $("#services1").click(function() {
+   $("#services1").slideDown().hide();
+   $("#design").show();
+   $("#design").click(function() {
+    $("#design").slideUp('1500');
+    $("#services1").slideDown('1500');
+   })
+  });
+  });
+  
