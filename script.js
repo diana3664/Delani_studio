793126796803dc1724dc2b9a65b5a0c55dd18216
diana3664 <dianamus2017@gmail.com>
2021-04-19 12:12:43 +0300

    add hover effect

--- script.js
+++ script.js
@@ -35,9 +35,38 @@ $(document).ready(function() {
 
 // portfolio
 $(document).ready(function() {
-$(".image7").hover(function(){
-  $("#api").show();(300);
+$(".image0").hover(function(){
+  $(".txt-hide0").fadeToggle(300)
 });
+
+$(".image1").hover(function(){
+  $(".txt-hide1").fadeToggle(300)
+});
+
+$(".image2").hover(function(){
+  $(".txt-hide2").fadeToggle(300)
+});
+
+$(".image3").hover(function(){
+  $(".txt-hide3").fadeToggle(300)
+});
+
+$(".image4").hover(function(){
+  $(".txt-hide4").fadeToggle(300)
+});
+
+$(".image5").hover(function(){
+  $(".txt-hide5").fadeToggle(300)
+});
+
+$(".image6").hover(function(){
+  $(".txt-hide6").fadeToggle(300)
+});
+
+$(".image7").hover(function(){
+  $(".txt-hide7").fadeToggle(300)
+});
+
 });
 
 $(document).ready(function(){
